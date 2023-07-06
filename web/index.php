e8ab9ad3c89df2807e37e7e5d624f3f7f8c0ddb6
harrysudanaa <komangharry02@gmail.com>
2023-07-06 15:41:44 +0800

    Add some styles

--- web/index.php
+++ web/index.php
@@ -1,25 +1,47 @@
 <?php
-echo "Nama saya Harry";
+// echo "Nama saya Harry";
 ?>
 
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Praktikum Docker</title>
+    <title>Identitas Diri</title>
     <style>
         body {
             font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
             margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            height: 100vh;
+            background-color: antiquewhite;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .wrapper {
+            width: 600px;
+            margin: 0 auto;
+            /* Center the container horizontally */
             padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
 
         h1 {
             text-align: center;
+            color: #333;
         }
 
         .info {
             margin-bottom: 20px;
+            padding-bottom: 10px;
+            border-bottom: 1px solid #ccc;
         }
 
         .info span {
@@ -29,20 +51,23 @@ echo "Nama saya Harry";
 </head>
 
 <body>
-    <h1>Identitas Diri</h1>
-    <h3>Praktikum Docker</h3>
+    <div class="container">
+        <div class="wrapper">
+            <h1>Identitas Diri</h1>
 
-    <div class="info">
-        <span>Name:</span> Komang Harry Sudana
-    </div>
+            <div class="info">
+                <span>Nama:</span> Komang Harry Sudana
+            </div>
 
-    <div class="info">
-        <span>NIM:</span> 2115101053
-    </div>
+            <div class="info">
+                <span>NIM:</span> 2115101053
+            </div>
 
-    <div class="info">
-        <span>Class:</span> IKI
+            <div class="info">
+                <span>Class:</span> IKI
+            </div>
+        </div>
     </div>
 </body>
 
-</html> -->
+</html>
