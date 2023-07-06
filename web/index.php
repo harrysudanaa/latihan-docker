7eb08962fee36dd150e92691d06895666bbe3c96
harrysudanaa <komangharry02@gmail.com>
2023-07-06 15:27:03 +0800

    first commit

---
+++ web/index.php
@@ -0,0 +1,48 @@
+<?php
+echo "Nama saya Harry";
+?>
+
+<!-- <!DOCTYPE html>
+<html>
+
+<head>
+    <title>Praktikum Docker</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .info {
+            margin-bottom: 20px;
+        }
+
+        .info span {
+            font-weight: bold;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Identitas Diri</h1>
+    <h3>Praktikum Docker</h3>
+
+    <div class="info">
+        <span>Name:</span> Komang Harry Sudana
+    </div>
+
+    <div class="info">
+        <span>NIM:</span> 2115101053
+    </div>
+
+    <div class="info">
+        <span>Class:</span> IKI
+    </div>
+</body>
+
+</html> -->
